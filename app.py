f64582ff13c67f0ef7a6624fe778e81af4f61add
Basar Akcin <33182504+basarakcin@users.noreply.github.com>
2023-07-31 12:51:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -17,22 +17,10 @@ def convert2grayscale(directory = 'src/expertise_pattern'):
         cv2.imwrite(image_path, grayscale_image)
 
 # convert2grayscale()
-
-def create_directory(directory_name):
-    try:
-        os.mkdir(directory_name)
-        print(f"Directory '{directory_name}' created successfully.")
-    except FileExistsError:
-        print(f"Directory '{directory_name}' already exists.")
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
-
-# Replace "build" with the desired directory name if you want a different one.
-directory_name = "build"
+os.mkdir("build")
 create_directory(directory_name)
 
 wait_for_active_window("New World")
-
 get_items()
 info = scrape_info(1, 10)
 for image_file_name, item_info in info.items():
