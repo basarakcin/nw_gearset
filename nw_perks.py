d9448398f1c127f6558e38a4c7fa1f047f7b14bf
Basar Akcin <33182504+basarakcin@users.noreply.github.com>
2023-07-31 10:50:35 +0200

    Create nw_perks.py

---
+++ nw_perks.py
@@ -0,0 +1,47 @@
+import requests
+from bs4 import BeautifulSoup
+
+GENERATED_PERKS_LIST_URL="https://nwdb.info/db/perks/generated/page/"
+
+def get_all_generated_perks():
+    for page_num in range(1, 9):
+        url = GENERATED_PERKS_LIST_URL + str(page_num)
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            # Process the data here, e.g., parse the response content
+            # or do whatever you need to do with the data.
+            print(f"Data from page {page_num}:")
+            print(response.text)
+        else:
+            print(f"Failed to fetch data from page {page_num}. Status code: {response.status_code}")
+    return
+
+        
+def get_perks_from_page(filename):
+    # Open the HTML file and parse it with BeautifulSoup
+    with open(filename, encoding='utf-8') as file:
+        soup = BeautifulSoup(file, "html.parser")
+
+    # Initialize an empty list to hold the perks
+    perks = []
+
+    # Find all 'td' elements with class 'ellipsis svelte-o9f32n'
+    for td in soup.find_all('td', {'class': 'ellipsis svelte-o9f32n'}):
+        # Find the 'a' tag within the 'td' tag
+        a = td.find('a', {'class': 'align-middle table-item-name color-rarity-0 svelte-o9f32n'})
+        # If an 'a' tag was found
+        if a:
+            # Get the text of the 'a' tag, remove newline characters, and add it to the perks list
+            perk = ' '.join(a.get_text().split())
+            perks.append(perk)
+
+    # Remove duplicates from the list
+    perks = list(set(perks))
+
+    return perks
+
+# Call the function and print the perks
+# perks = get_perks_from_page("nwdb.html")
+# print(perks)
+get_all_generated_perks()
